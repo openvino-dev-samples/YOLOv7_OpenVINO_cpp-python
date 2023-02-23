d41fe941208e03f2afcae926d9527692fcf679ab
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-02-23 16:37:56 +0530

    Update yolov7.py

--- python/yolov7.py
+++ python/yolov7.py
@@ -171,7 +171,10 @@ class YOLOV7_OPENVINO(object):
     
     def draw(self, img, boxinfo):
         for xyxy, conf, cls in boxinfo:
-            self.plot_one_box(xyxy, img, label=self.classes[int(cls)], color=self.colors[int(cls)], line_thickness=2)
+            
+            score = f"{conf:.2f}"
+            label_text = f"{self.classes[int(cls)]} {score}"
+            self.plot_one_box(xyxy, img, label=label_text, color=self.colors[int(cls)], line_thickness=2)
         cv2.imshow('Press ESC to Exit', img) 
         cv2.waitKey(1)
 
